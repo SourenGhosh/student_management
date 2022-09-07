3b4e5e75308a85ce2ba47441b27b6cc0bb5a6f2b
souren ghosh <souren94ghosh@gmail.com>
2022-09-07 15:29:57 +0530

    search

--- student_management/backend_core/views.py
+++ student_management/backend_core/views.py
@@ -4,14 +4,27 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.views import View
-
+from django.db.models import Q
 from django.urls import reverse
+
+from backend_core.models import Student
 
 
 class StudentView(View):
     def get(self, request, *args, **kwargs):
+        qs = Student.objects.all()
         return render(
             request,
-            'backend_core/index.html'
+            'backend_core/index.html',
+            {"qs": qs},
         )
-        
+    def post(self, request, *args, **kwargs):
+        search_string = request.POST.get('search')
+        search_params = Q(email__icontains=search_string) | Q(first_name__icontains=search_string) \
+            | Q(last_name__icontains = search_string)
+        qs = Student.objects.filter(search_params)
+        return render(
+            request,
+            'backend_core/index.html',
+            {"qs": qs},
+        )
