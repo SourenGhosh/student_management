e6720fb75d8c58395699662ccc30898311a21195
Ubuntu <ubuntu@ip-172-31-3-230.ap-south-1.compute.internal>
2022-09-08 11:03:41 +0000

    ch settings

--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4cvljwgblb^^d@d51(fjc-aa$vsd+5z_676hjmb%ln_h2igbh%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
